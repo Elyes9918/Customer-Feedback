6651714da64d5734b3f7642aeeffad74feb93a0d
Elyes9918 <elyes9918@gmail.com>
2023-03-06 17:58:29 +0100

    User Dtos and Services

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\FeedbackRepository;
-use App\Repository\ProjectRepository;
-use App\Repository\UserRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use App\DataTransferObjects\UserDto;
+use App\Services\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -18,158 +14,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
 
-    public function __construct(
-    private UserRepository $userRepository,
-    private ManagerRegistry $doctrine,
-    private ProjectRepository $projectRepository,
-    private FeedbackRepository $feedbackRepository)
-    {    
-
-    }
+    public function __construct(private UserService $userService)
+    {    }
 
 
     #[Route('/users', name: 'app_users_get', methods: "GET")]
     public function index(): JsonResponse
-    {
-        $users = $this->userRepository->findAll();
-        $data = [];
-    
-    
-        foreach ($users as $user) {
-
-            $projects = [];
-            $feedbacks = [];
-
-            foreach($user->getProjects() as $project){
-               // $projects[] = ['id' => $project->getId() ]; //this is an object 
-               $projects[] = $project->getId();
-            }
-
-            foreach($user->getFeedbacks() as $feedback){
-                $feedbacks[] = $feedback->getId() ;
-            }
-
- 
-            $data[] = [
-                'id' => $user->getId(),
-                'email' => $user->getEmail(),
-                'created_at'=>$user->getCreatedAt()->format('Y-m-d H:i:s'),
-                'modified_at'=> $user->getModifiedAt()->format('Y-m-d H:i:s'),
-                'first_name'=> $user->getFirstName(),
-                'last_name'=>$user->getLastName(),
-                'birth_date'=>$user->getBirthDate(),
-                'status'=>$user->getStatus(),
-                'address'=>$user->getAddress(),
-                'phone_number'=>$user->getPhoneNumber(),
-                'company'=>$user->getCompany(),
-                'projects_id'=>$projects,
-                'feedbacks_id'=>$feedbacks,
-                'roles'=> $user->getRoles(),
-            ];
-        }
-
-        return $this->json($data);
+    {   
+        $userDtos = $this->userService->listAllUsers();
+       
+        return $this->json($userDtos);
     }
+
 
     #[Route('/users/{id}', name: 'app_user_get', methods: "GET")]
     public function getUserById(int $id): JsonResponse
     {
-        $user = $this->userRepository->find($id);
-
-        $data = [];
-        $projects = [];
-        $feedbacks = [];
-
-        foreach($user->getProjects() as $project){
-            // $projects[] = ['id' => $project->getId() ]; //this is an object 
-            $projects[] = $project->getId();
-        }
-
-        foreach($user->getFeedbacks() as $feedback){
-            $feedbacks[] = $feedback->getId() ;
-        }
-
-
-        $data = [
-            'id' => $user->getId(),
-            'email' => $user->getEmail(),
-            'created_at'=>$user->getCreatedAt()->format('Y-m-d H:i:s'),
-            'modified_at'=> $user->getModifiedAt()->format('Y-m-d H:i:s'),
-            'first_name'=> $user->getFirstName(),
-            'last_name'=>$user->getLastName(),
-            'birth_date'=>$user->getBirthDate(),
-            'status'=>$user->getStatus(),
-            'address'=>$user->getAddress(),
-            'phone_number'=>$user->getPhoneNumber(),
-            'company'=>$user->getCompany(),
-            'projects_id'=>$projects,
-            'feedbacks_id'=>$feedbacks,
-            'roles'=> $user->getRoles(),
-            ]
-        ;
+        $userDto = new UserDto();
+        $userDto = $this->userService->getUserById($id);
         
-
-        return $this->json($data);
+        return $this->json($userDto);
     }
 
+
     #[Route('/users/{id}', name: 'app_users_patch', methods: "PATCH")]
-    public function update(Request $request,int $id): JsonResponse
+    public function updateUser(Request $request,int $id): JsonResponse
     {
         
         if ($id == null) {
             return new JsonResponse("id is incorrect", 200, [], true);
         }
 
-        $user = $this->userRepository->find($id);
-
-
-        $data = json_decode($request->getContent(), true);
-
-        if (isset($data['email'])) { $user->setEmail($data['email']);  }
-
-        if (isset($data['first_name'])) { $user->setFirstName($data['first_name']);  }
-
-        if (isset($data['last_name'])) { $user->setLastName($data['last_name']); }
-
-        if (isset($data['birth_date'])) { $user->setBirthDate($data['birth_date']); }
-
-        if (isset($data['status'])) { $user->setStatus($data['status']); }
-
-        if (isset($data['address'])) { $user->setAddress($data['address']); }
-
-        if (isset($data['phone_number'])) { $user->setPhoneNumber($data['phone_number']); }
-
-        if (isset($data['company'])) { $user->setCompany($data['company']); }
-
-        if (isset($data['project_id'])) { 
-            //Logic to assign a single project with an id passed 
-            $project = $this->projectRepository->find($data['project_id']);
-            $user->addProject($project);
-        }
-
-        if (isset($data['feedback_id'])) {
-            //Logic to assign a signle feedback with an id passed
-            $feedback = $this->feedbackRepository->find($data['feedback_id']);
-            $user->addFeedback($feedback);
-        }
-
-        if (isset($data['roles'])) { 
-            //Logic to a assign multiple roles with the name passed
-            $roles = $user->getRoles();
-        
-            foreach($data['roles'] as $role){
-            array_push($roles,$role);
-            }
-
-            $user->setRoles($roles); 
-        }
-    
-        $entityManger = $this->doctrine->getManager();
-        $entityManger->flush();
+        $this->userService->updateUser($request,$id);
 
         return new JsonResponse(['meesage' => 'User updated succesfully'], 200);
-
     }
+
 
 
      #[Route('/users/{id}/role', name:'app_user_role_unassign', methods: "PATCH")]
@@ -179,24 +59,11 @@ class UserController extends AbstractController
             return new JsonResponse("id is incorrect", 200, [], true);
         }
 
-        $user = $this->userRepository->find($id);
-
-        $data = json_decode($request->getContent(), true);
-
-        $roles = $user->getRoles();
-
-        $index = array_search($data['role'],$roles);
-        if($index !== FALSE){
-            unset($roles[$index]);
-        }
-
-        $user->setRoles($roles);
-
-        $entityManger = $this->doctrine->getManager();
-        $entityManger->flush();
+        $this->userService->unAssignRole($request,$id);
 
         return new JsonResponse(['message'=>'Role Unassigned Successfuly'],200);
      }
+
 
     #[Route('/users/{id}/project', name:'app_user_project_unassign', methods: "PATCH")]
     public function unAssignProject(Request $request,int $id):JsonResponse{
@@ -205,26 +72,7 @@ class UserController extends AbstractController
             return new JsonResponse("id is incorrect", 200, [], true);
         }
 
-        $user = $this->userRepository->find($id);
-
-        $data = json_decode($request->getContent(), true);
-
-        //Logic to remove a project from a user
-        //Automatically remove all feedbacks related to that project from this user
-        //I have to get all the feedbacks related to the project then unassign them from the user
-        
-        $feedbacks = $this->feedbackRepository->findBy(array("project"=>$data['project_id']));
-        foreach($feedbacks as $feedback){
-            $user->removeFeedback($feedback);
-        }
-
-        $project = $this->projectRepository->find($data['project_id']);
-        $user->removeProject($project);
-        
-
-
-        $entityManger = $this->doctrine->getManager();
-        $entityManger->flush();
+        $this->userService->unAssignProject($request,$id);
 
         return new JsonResponse(['message'=>'Project Unassigned Successfuly'],200);
     }
@@ -237,34 +85,10 @@ class UserController extends AbstractController
             return new JsonResponse("id is incorrect", 200, [], true);
         }
 
-        $user = $this->userRepository->find($id);
-
-        $data = json_decode($request->getContent(), true);
-
-        //Logic to remove a feedback from a user
-        $feedback = $this->feedbackRepository->find($data['feedback_id']);
-        $user->removeFeedback($feedback);
-
-        $entityManger = $this->doctrine->getManager();
-        $entityManger->flush();
+        $this->userService->unAssignFeedBack($request,$id);
 
         return new JsonResponse(['message'=>'Feedback Unassigned Successfuly'],200);
     }
 
 
-    #[Route('/users/{id}', name: 'app_user_delete', methods: "DELETE")]
-    public function deleteUser(int $id): JsonResponse
-    {
-        $user = $this->userRepository->find($id);
-
-        if ($user) {
-            $entityManger = $this->doctrine->getManager();
-            $entityManger->remove($user);
-            $entityManger->flush();
-            return new JsonResponse(['meesage' => 'User deleted succesfully'], 200);
-        }
-
-        return new JsonResponse(['message' => 'User not found'], 404);
-    }
-    
 }
--- src/DataTransferObjects/UserDto.php
+++ src/DataTransferObjects/UserDto.php
@@ -2,25 +2,278 @@
 
 namespace App\DataTransferObjects;
 
-use DateTime;
 
-class UserDto{
+class UserDto {
 
     private ?int $id = null;
     private ?string $email=null;
-    private ?DateTime $created_at=null;
-    private ?DateTime $modified_at=null;
-    private ?string $first_name=null;
-    private ?string $last_name=null;
-    private ?string $birth_date=null;
+    private ?String $createdAt=null;
+    private ?String $modifiedAt=null;
+    private ?string $firstName=null;
+    private ?string $lastName=null;
+    private ?string $birthDate=null;
     private ?string $status=null;
     private ?string $address=null;
-    private ?string $phone_number=null;
+    private ?string $phoneNumber=null;
     private ?string $company=null;
-    private array $projects_id= [];
-    private array $feedbacks_id= [];
+    private array $projectsId= [];
+    private array $feedbacksId= [];
     private array $roles= [];
 
     
+    public function __construct(
+    ) {}
+   
 
+    /**
+     * Get the value of id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     */
+    public function getCreatedAt(): ?String
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     */
+    public function setCreatedAt(?String $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of modifiedAt
+     */
+    public function getModifiedAt(): ?String
+    {
+        return $this->modifiedAt;
+    }
+
+    /**
+     * Set the value of modifiedAt
+     */
+    public function setModifiedAt(?String $modifiedAt): self
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstName
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     */
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     */
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of birthDate
+     */
+    public function getBirthDate(): ?string
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set the value of birthDate
+     */
+    public function setBirthDate(?string $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of status
+     */
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     */
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of address
+     */
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set the value of address
+     */
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of phoneNumber
+     */
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * Set the value of phoneNumber
+     */
+    public function setPhoneNumber(?string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of company
+     */
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set the value of company
+     */
+    public function setCompany(?string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of projectsId
+     */
+    public function getProjectsId(): array
+    {
+        return $this->projectsId;
+    }
+
+    /**
+     * Set the value of projectsId
+     */
+    public function setProjectsId(array $projectsId): self
+    {
+        $this->projectsId = $projectsId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of feedbacksId
+     */
+    public function getFeedbacksId(): array
+    {
+        return $this->feedbacksId;
+    }
+
+    /**
+     * Set the value of feedbacksId
+     */
+    public function setFeedbacksId(array $feedbacksId): self
+    {
+        $this->feedbacksId = $feedbacksId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of roles
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set the value of roles
+     */
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
 }
